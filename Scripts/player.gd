f4d547d5fe6e0e3f3d6a0d80d04722e41218c761
maskenary <125152643+maskenary@users.noreply.github.com>
2024-10-09 10:41:58 -0700

    player w/ dodge and shoot

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,43 +3,56 @@ extends CharacterBody2D
 signal bullet_fired(bullet_instance)
 
 const bullet = preload("res://Scenes/Bullet.tscn")
+var shoot_cd = 0.2
 var can_shoot = true
-var speed = 500
+var dodge_speed = 800
+var normal_speed = 400
+var input_direction = Vector2.ZERO
+
 enum States {
 	ACTIVE,
 	DODGING
 }
 var state = States.ACTIVE
 @onready var screen_size = get_viewport().size
-@onready var sprite_width_scaled = $Sprite2D.texture.get_width() * transform.get_scale().x
-@onready var sprite_height_scaled = $Sprite2D.texture.get_height() * transform.get_scale().y
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-func move():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
-	move_and_slide()
+@onready var sprite = $Sprite2D
+@onready var sprite_width_scaled = sprite.texture.get_width() * self.transform.get_scale().x
+@onready var sprite_height_scaled = sprite.texture.get_height() * self.transform.get_scale().y
+@onready var anim = $AnimationPlayer
 	
 func shoot():
-	if Input.is_action_pressed("ui_accept") and can_shoot:
+	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = bullet.instantiate()
 		bullet_instance.global_position = self.global_position
 		emit_signal("bullet_fired", bullet_instance)
 		can_shoot = false
-		await get_tree().create_timer(0.2).timeout
+		await get_tree().create_timer(shoot_cd).timeout
 		can_shoot = true
+		
+func dodge():
+	if Input.is_action_pressed("dodge") and input_direction != Vector2.ZERO:
+		anim.play("dodge")
+		state = States.DODGING
+		
 
+func _process(delta: float) -> void:
+	if state == States.ACTIVE:
+		dodge()
+		shoot()
+	if state == States.DODGING:
+		sprite.modulate = Color(0.282, 0.612, 1)
+		if !anim.is_playing():
+			sprite.modulate = Color(1, 1, 1)
+			state = States.ACTIVE
+			
 func _physics_process(delta):
 	if state == States.ACTIVE:
-		move()
-		shoot()
+		input_direction = Input.get_vector("left", "right", "up", "down")
+		velocity = input_direction * normal_speed 
+		move_and_slide()
+	if state == States.DODGING:
+		velocity = input_direction * dodge_speed 
+		move_and_slide()
 		
 	# Clamp the player to the screen
 	position.x = clamp(position.x, 0+sprite_width_scaled/2, screen_size.x-sprite_width_scaled/2)
