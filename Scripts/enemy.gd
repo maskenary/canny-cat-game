fa39c6b5e883246014ecc6a4335c4458404dd7a6
maskenary <maskenary@gmail.com>
2024-11-05 16:48:28 -0800

    enemy spawning not fixed

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,15 +3,45 @@ extends PathFollow2D
 signal spawn_pattern(pattern)
 
 var pattern_spawner = load("res://Scenes/Boss/pattern_spawner.tscn")
+var single_spin_formation = load("res://Scenes/Boss/single_spin_formation.tscn")
 var active = false
+var attack_cooldown = 3
+var target_position = null
+var path_parent = null
+var pre_active_speed = 10
+var active_speed = 300
 
-func _ready() -> void:
-	pass
 
-"""
+func setup(initial_progress, offset: Vector2, path: Node):
+	self.progress = initial_progress
+	self.h_offset = offset.x
+	self.v_offset = offset.y
+	path_parent = path
+
+func _process(delta: float) -> void:
+	if active:
+		self.progress += active_speed
+		#await get_tree().create_timer(attack_cooldown).timeout
+		#attack()
+
+func _physics_process(delta: float) -> void:
+	# Gradually un-offset until reaching starting point, then begin pathing
+	if !active:
+		"""
+		if self.h_offset != 0:
+			self.h_offset -= pre_active_speed * sign(self.h_offset)
+		if self.v_offset != 0:
+			self.v_offset -= pre_active_speed * sign(self.v_offset)
+		"""
+		# TODO fix the initial spawn -> following path process
+		if self.h_offset == 0 and self.v_offset == 0:
+			get_parent().remove_child(self)
+			path_parent.add_child(self)
+			active = true
+		
+
 func attack():
 	var pattern = pattern_spawner.instantiate()
 	pattern.position = self.position
 	emit_signal("spawn_pattern", pattern)
 	pattern.shotgun_at_player(single_spin_formation, 300, 6, 0.5, 90, 3, 10)
-"""
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -13,7 +13,8 @@ var boss_interface_instance
 @export var boss_spawn: Node
 @export var boss_positions: Array[Node]
 @export var enemy_path: Node
-@export var enemy_spawnpoint: Node
+@export var enemy_spawn: Node
+@export var enemy_initial: Node
 
 var level_progress = 1
 
@@ -52,7 +53,7 @@ func _process(delta: float) -> void:
 	if level_progress == 2:
 		spawn_boss()
 		level_progress += 1
-	
+
 func _on_player_damage_taken(hp) -> void:
 	emit_signal("update_healthbar", hp)
 	
@@ -62,6 +63,9 @@ func _on_player_charges_changed(charges) -> void:
 func _on_spawn_timer_timeout() -> void:
 	var e = enemy.instantiate()
 	e.spawn_pattern.connect(spawn_pattern)
-	enemy_spawnpoint.progress_ratio = randf()
-	e.position = enemy_spawnpoint.global_position
+	enemy_initial.progress_ratio = randf()
+	enemy_spawn.progress_ratio = randf()
+	var offset_x = -(enemy_initial.position.x - enemy_spawn.position.x)
+	var offset_y = -(enemy_initial.position.y - enemy_spawn.position.y)
+	e.setup(enemy_initial.progress_ratio, Vector2(offset_x, offset_y), enemy_path)
 	add_child(e)
